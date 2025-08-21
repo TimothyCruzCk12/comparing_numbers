33652fd81db97bafd9780ab56c533eea2512abb2
User <user@example.com>
2025-08-21 09:40:25 -0700

    fixed deploy issue

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/comparing-numbers/',
+  base: '/comparing_numbers/',
 })
